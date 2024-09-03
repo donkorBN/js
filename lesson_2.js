ac77a95d67b39e5fd7fbb465b5c5bfd59ad7240c
donkorBN <projectmanuel89@gmail.com>
2024-09-03 11:18:40 +0000

    exercises

--- lesson_2.js
+++ lesson_2.js
@@ -7,3 +7,12 @@ const name = "My name is: "
 let name2 = "donkorBN"
 
 console.log(name)
+
+console.log(name + "" + name2)
+
+const order = {
+    coffee: 1,
+    bagel:1
+};
+
+console.log(order.bagel)
