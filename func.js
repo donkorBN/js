b7a48cee225dfdccb7f0f5c0e2c155993a6cc9fe
donkorBN <projectmanuel89@gmail.com>
2024-09-04 11:59:33 +0000

    replacing boilerplate game code with functions

--- func.js
+++ func.js
@@ -1,4 +1,4 @@
-function add(x, y){
+/*function add(x, y){
     return x+y
 }
 
@@ -7,4 +7,10 @@ y = parseInt(prompt("Input an integer"), 10)
 
 let result = "The answer is " + add(x, y)
 
-console.log(result)
+console.log(result) */
+
+function tax(price, percent) {
+    return price * percent
+}
+
+console.log(tax(1000, 0.8))
