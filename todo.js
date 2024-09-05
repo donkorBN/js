5f9ab7a47337cff02a8e43aaac90e4a2e1ba9ae2
donkorBN <projectmanuel89@gmail.com>
2024-09-05 14:02:46 +0000

    add

--- todo.js
+++ todo.js
@@ -1,6 +1,18 @@
 const todoList = []
 
+function displayTask() {
 
+    let todoListHTML = ''
+    for (let i = 0; i < todoList.length; i++) {
+        const todo = todoList[i]
+        const html = `<p>${todo}</p>`
+        todoListHTML += html
+    }
+    document.querySelector('.js-todo-list').innerHTML = todoListHTML;
+    console.log(todoListHTML)
+}
+
+displayTask()
 function addTodo(){
     const inputElement = document.querySelector('.input_field');
     const taskName = inputElement.value;
@@ -13,19 +25,5 @@ function addTodo(){
 
     inputElement.value = '';
     displayTask()
-    }
-
-function displayTask() {
-
-    let todoListHTML = ''
-    for (let i = 0; i < todoList.length; i++) {
-        const todo = todoList[i]
-        const html = `${todo}`
-        todoListHTML += html
-    }
-    document.querySelector('.js-todo-list').innerHTML = todoListHTML;
-    console.log(todoListHTML)
 }
-
-
 
