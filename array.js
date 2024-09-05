eea6202f1981ced34f9564c8c34592a2e2da257a
donkorBN <projectmanuel89@gmail.com>
2024-09-05 14:30:09 +0000

    array-push

--- array.js
+++ array.js
@@ -5,7 +5,7 @@
 for (let i = 0; index < todoList.length; i++){
     const value = todoList.[i];
     console.log(value)
-} */
+}
 
 const nums = [4,4,3,3,5,5,31,6,]
 
@@ -23,4 +23,35 @@ for (let i = 0; i < nums.length; i++) {
     nums[i] = num *2
 }
 
+//11a)
 console.log((nums))
+
+const arr = [10, 20, 30]
+
+arr[2] = 90
+
+console.log(arr) */
+
+//11b
+
+function getLastValue(arr = []){
+    lastIndex = arr[arr.length -1];
+    console.log(lastIndex)
+}
+
+getLastValue([2,4,6])
+getLastValue(['hi','hello','world'])
+
+function arraySwap(arr = []){
+    let firstValue = arr[0];
+    let lastValue = arr[arr.length -1];
+
+    [firstValue, lastValue] = [lastValue, firstValue];
+
+    arr[0] = firstValue;
+    arr[arr.length -1] = lastValue;
+
+    console.log(arr)
+}
+
+arraySwap([1,2,3,4,5,6])
