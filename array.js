5500f2b747f540372350b5f59b143d7879d302ad
donkorBN <projectmanuel89@gmail.com>
2024-09-05 13:22:38 +0000

    new

--- array.js
+++ array.js
@@ -1,8 +1,16 @@
-const todoList = [
+/*const todoList = [
     'make lunch', 'wash clothes','watch ironman'
 ]
 
-for (let index = 0; index <= todoList.length -1; index++){
-    todoList.[index]
-    console.log(index)
+for (let i = 0; index < todoList.length; i++){
+    const value = todoList.[i];
+    console.log(value)
+} */
+
+const nums = [4,4,3,3,5,5,31,6,]
+
+for (let i = 0; i < nums.length; i++) {
+    const num = nums[i]
+    sum += num
 }
+console.log(sum)
