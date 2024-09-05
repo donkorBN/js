b4607232afdf772118cb91d01e45922ffa3e21dd
donkorBN <projectmanuel89@gmail.com>
2024-09-05 12:34:08 +0000

    todo added

---
+++ todo.js
@@ -0,0 +1,9 @@
+const todoList = []
+
+let inputValue = input.value
+
+function addTodo(){
+    document.querySelector('.js-button').addEventListener('click', (e) => {
+        todo.push(input.value)
+    })
+}
