88f58dbfb637239d5572dd3b940dd45fcb6b0316
donkorBN <projectmanuel89@gmail.com>
2024-09-05 13:13:35 +0000

    arr

---
+++ array.js
@@ -0,0 +1,8 @@
+const todoList = [
+    'make lunch', 'wash clothes','watch ironman'
+]
+
+for (let index = 0; index <= todoList.length -1; index++){
+    todoList.[index]
+    console.log(index)
+}
