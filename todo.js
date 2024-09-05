abaf72e60bc573793e2ebf80b363ebeba5acfc53
donkorBN <projectmanuel89@gmail.com>
2024-09-05 12:42:38 +0000

    touches

--- todo.js
+++ todo.js
@@ -3,7 +3,9 @@ const todoList = []
 let inputValue = input.value
 
 function addTodo(){
-    document.querySelector('.js-button').addEventListener('click', (e) => {
-        todo.push(input.value)
-    })
-}
+    const inputElement = document.querySelector('.js-button');
+    const taskName = String(document.querySelector('.input_field'))
+    const task = taskName.va
+    todoList.push(taskName)
+    console.log(todoList)
+    }
