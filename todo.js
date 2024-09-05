f8bf1728d36efcd48685e6a291b9318e47ec134f
donkorBN <projectmanuel89@gmail.com>
2024-09-05 12:45:42 +0000

    fix -f

--- todo.js
+++ todo.js
@@ -3,9 +3,12 @@ const todoList = []
 let inputValue = input.value
 
 function addTodo(){
-    const inputElement = document.querySelector('.js-button');
-    const taskName = String(document.querySelector('.input_field'))
-    const task = taskName.va
+    const inputElement = document.querySelector('.input_field');
+    const taskName = inputElement.value;
+
+    //const inputField = document.querySelector('.price');
+    //let cost = Number(inputField.value)
+
     todoList.push(taskName)
     console.log(todoList)
     }
